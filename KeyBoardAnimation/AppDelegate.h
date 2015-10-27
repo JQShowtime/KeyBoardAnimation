e65ff5a2fe6e238dee92790d6590f75416903268
hjq <413940035@qq.com>
2015-10-27 14:35:29 +0800

    Initial Commit

---
+++ KeyBoardAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyBoardAnimation
+//
+//  Created by roboca on 15/10/27.
+//  Copyright © 2015年 roboca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
